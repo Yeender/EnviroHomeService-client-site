67c1c01841626c535da6e9194ab08de0bd9cc83a
Md-Mahbub-Hasan <trylife.mahbub@gmail.com>
2021-03-11 08:46:08 -0800

    @Mahbub-Hasan1

--- components/Home/Header.js
+++ components/Home/Header.js
@@ -1,27 +1,83 @@
 import SwiperCore, { A11y, Autoplay, Navigation, Pagination, Scrollbar } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/swiper-bundle.css';
+// import backgroundVideo from '../../rsc/Header.mp4';
 
 const Header = () => {
-    SwiperCore.use([Navigation, Pagination, Scrollbar, Autoplay, A11y]);
-    return (
-        <Swiper
-      spaceBetween={50}
-      slidesPerView={3}
-      navigation
-      pagination={{ clickable: true }}
-      scrollbar={{ draggable: false }}
-      autoPlay={true}
-      onSwiper={(swiper) => console.log(swiper)}
-      onSlideChange={() => console.log('slide change')}
-    >
-      <SwiperSlide>Slide 1</SwiperSlide>
-      <SwiperSlide>Slide 2</SwiperSlide>
-      <SwiperSlide>Slide 3</SwiperSlide>
-      <SwiperSlide>Slide 4</SwiperSlide>
-      ...
-    </Swiper>
-    );
+  SwiperCore.use([Navigation, Pagination, Scrollbar, Autoplay, A11y]);
+  return (
+    <div>
+      <Swiper
+        spaceBetween={50}
+        slidesPerView={1}
+        navigation
+        pagination={{ clickable: true }}
+        // scrollbar={{ draggable: true }}
+        onSwiper={(swiper) => console.log(swiper)}
+        onSlideChange={() => console.log('slide change')}
+      >
+        <SwiperSlide>
+
+          <header className="d-flex align-items-center">
+            <div className="container-xl">
+              <div className="header-contents">
+                <h2>GET A QUOTE RIGHT NOW ON <br /> YOUR NEXT RENOVATION!</h2>
+                <button>Contact Us</button>
+              </div>
+            </div>
+          </header>
+
+        </SwiperSlide>
+        <SwiperSlide>
+          <div>
+            <div className="slider-heder2 d-flex align-items-center">
+              <div className="container-xl">
+                <div className="slider-heder2-contents">
+                  <h2>GET A QUOTE RIGHT NOW ON <br /> YOUR NEXT RENOVATION!</h2>
+                  <button>Contact Us</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+        <div>
+            <div className="slider-heder3 d-flex align-items-center">
+              <div className="container-xl">
+                <div className="slider-heder2-contents">
+                  <h2>GET A QUOTE RIGHT NOW ON <br /> YOUR NEXT RENOVATION!</h2>
+                  <button>Contact Us</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+
+      </Swiper>
+      <style jsx>{`
+    .slider-heder2{
+      background: url("https://i.imgur.com/3cI7CH8.jpg") 0 0 / cover;
+      background-repeat: no-repeat;
+      background-position: center;
+      height: 650px;
+    }
+    .slider-heder3{
+      background: url("https://i.imgur.com/2Gf2oOd.jpg") 0 0 / cover;
+      background-repeat: no-repeat;
+      background-position: center;
+      height: 600px;
+    }
+
+    .slider-heder2-contents h2{
+      font-size: 50px;
+  font-weight: 900;
+  color: #1A5464;
+  text-shadow: 2px 2px #7F020B;
+    }
+    `}</style>
+    </div>
+  );
 };
 
 export default Header;
